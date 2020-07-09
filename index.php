7171b02108c7af8c877c218000fd6b131f23f1f6
François Delapierre <francois.delapierre@youscribe.com>
2020-07-09 15:10:04 +0200

    add date value by default v2

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ $liste_urls = [];
   <h2>GnakryLive</h2>
   <form method="POST" action="generatePDF.php">
     <label for="editeur">Nom du journal : </label><input type="text" name="editeur" id="editeur" value="GnakryLive"><br />
-    <label for="date">Date de parution : </label><input type="date" name="date" id="date" value="<?php echo date("y-m-d");?>"><br />
+    <label for="date">Date de parution : </label><input type="date" name="date" id="date" value="<?php echo date("Y-m-d");?>"><br />
     <label for="urls">URLs des articles : </label><textarea name="urls" id="urls" cols="100" rows="20" ><?php $urls = $html->find('.nav-stacked li a');
     for($x=0;$x<10;$x++)
     {
